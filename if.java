ab59c3d7eb0e6e9914db582013edf55446aa8182
Parth Parsana <145137359+ParthParsana@users.noreply.github.com>
2024-07-20 18:20:31 +0530

    Add files via upload

---
+++ if.java
@@ -0,0 +1,17 @@
+class ife
+{
+   public static void main (String x[])
+   
+   {
+      int a=10;
+	  
+	  if (a>=50)
+	    {
+	      System.out.println("a is greaterthen");
+	    }
+	 else
+	    {
+	      System.out.println("a is lessthen");
+	    }
+   }
+}
