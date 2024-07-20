4f3e92e15e8842dc0d103c29219cc048d8033289
Parth Parsana <145137359+ParthParsana@users.noreply.github.com>
2024-07-20 18:21:17 +0530

    Add files via upload

---
+++ elseif.java
@@ -0,0 +1,24 @@
+class elseif
+{
+		public static void main(String a[])
+		{
+		   int time = 5;
+		   
+		   if (time < 10 )
+		   {
+			   System.out.println("Good Morning");
+		   }
+		   else if(time < 15)
+		   {
+			   System.out.println("Good Afternoon");
+		   }
+		   else if(time <20 )
+		   {
+			   System.out.println("Good Evening");
+		   }
+		   else 
+		   {
+			   System.out.println("Good Night");
+		   }
+		}
+}
