44c6bc03126bd3318242441dfa51132c7c0754a6
Parth Parsana <145137359+ParthParsana@users.noreply.github.com>
2024-07-21 20:27:22 +0530

    Add files via upload

---
+++ abstractt.java
@@ -0,0 +1,24 @@
+abstract class a
+{
+	abstract void disp();
+	{
+	  
+	}
+	
+}
+class b extends a
+{
+    void disp()
+	{
+	  System.out.println("hiii");
+	}
+}
+class abstractt
+{
+	public static void main(String ar[])
+	{
+	  b s1=new b();
+	   s1.disp();
+	 
+	}
+}
