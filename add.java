62d295da62857ad81ea207c11eea126adbf6d14c
Parth Parsana <145137359+ParthParsana@users.noreply.github.com>
2024-07-21 20:27:41 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,17 @@
+class add
+{
+   public static void main(String x[])
+     
+   {
+	   int choice,a=10,b=20,c;
+	   System.out.println("addition of two number");
+		c=a+b;
+        System.out.println("C :"+c);
+		System.out.println("multiply of two number");
+		c=a*b;
+        System.out.println("C :"+c);
+		System.out.println("division of two number");
+		c=a/b;
+        System.out.println("C :"+c);
+   }
+}
