e5c7ae43217e990efab2cdfa936a3231a7ae6df5
Parth Parsana <145137359+ParthParsana@users.noreply.github.com>
2024-07-23 18:45:30 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,14 @@
+class facto
+
+{
+   public static void main(String x[])
+   {
+    int i,fact=1;
+      int number=5;
+    for(i=1;i<=number;i++)
+	  {
+	     fact = fact*i;
+	  }
+	  System.out.println("factorial = "+fact);
+   }
+}
