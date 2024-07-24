d3134aeb53d30b0dff69a28ccc6e8ef0c9fc9205
Parth Parsana <145137359+ParthParsana@users.noreply.github.com>
2024-07-24 15:42:45 +0530

    Add files via upload

---
+++ garbage.java
@@ -0,0 +1,19 @@
+class a
+{
+	void finalized()
+	{
+		System.out.println("object Delete");
+	}
+}
+
+class garbage
+{
+		public static void main(String a[])
+		{
+			a s1=new a();
+			s1.finalized();
+			s1= null;
+			System.gc();
+			
+		}
+}
