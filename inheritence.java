8e5dd673e4e0fb8bbeca29ea5fd82e060b0101aa
Parth Parsana <145137359+ParthParsana@users.noreply.github.com>
2024-07-25 15:46:28 +0530

    Add files via upload

---
+++ inheritence.java
@@ -0,0 +1,37 @@
+
+
+class a
+{
+	void disp()
+
+	{
+       System.out.println("hi....");
+	}
+}
+class b extends a
+{
+	void disp1()
+
+	{
+       System.out.println("byy....");
+	}
+}
+class c extends b
+{
+	void disp2()
+
+	{
+       System.out.println("go....");
+	}
+}
+ class c_main
+{
+	public static void main(String ar[])
+   {
+	c s=new c();
+	s.disp2();
+	s.disp1();
+	s.disp();
+
+   }
+}
