2366ede7bb5e2dd8234270af11be88b7af82bec1
Parth Parsana <145137359+ParthParsana@users.noreply.github.com>
2024-07-27 14:37:34 +0530

    Add files via upload

---
+++ interest.java
@@ -0,0 +1,13 @@
+class interest
+{
+public static void main(String x[])
+
+   {
+       float si,p=1,r=1,n=1;
+	   
+	   si = (p*r*n) / 100;
+	   
+	   System.out.println("Simple Interest : " + si);
+   
+   }
+}
