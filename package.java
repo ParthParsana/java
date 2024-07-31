d3617e407cbd303bdc190c3bc7f146a5782487e3
Parth Parsana <145137359+ParthParsana@users.noreply.github.com>
2024-07-31 09:22:50 +0530

    Add files via upload

---
+++ package.java
@@ -0,0 +1,15 @@
+import java.util.*; 
+class packages
+{
+	public static void main(String ar[])
+	{
+
+      /* Date s1=new Date();
+       System.out.println("DATE :"+s1);*/
+		
+		Scanner s1 = new Scanner(System.in);  
+		System.out.println("Enter the first name :");
+        int a = s1.nextInt();
+        System.out.println("name :"+a);
+	}
+}
