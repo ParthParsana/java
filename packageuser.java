faf8c81904578f7cc5905cb059e2ebec0d554265
Parth Parsana <145137359+ParthParsana@users.noreply.github.com>
2024-08-01 08:47:41 +0530

    Add files via upload

---
+++ packageuser.java
@@ -0,0 +1,16 @@
+
+ class abc
+ {
+ 
+    public void disp()
+    {
+        System.out.println("Hello");
+    }
+    import student.abc;
+ 
+    public static void main(String args[])
+    {
+        abc s1 = new abc();
+         s1.disp();
+    }
+}
