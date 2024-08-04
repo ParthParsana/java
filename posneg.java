a6bc5e4c6c705a70e129b85044dd11cd4f79234f
Parth Parsana <145137359+ParthParsana@users.noreply.github.com>
2024-08-04 12:29:04 +0530

    Add files via upload

---
+++ posneg.java
@@ -0,0 +1,17 @@
+class posneg
+{
+   public static void main (String x[])
+   
+   {
+      int a=-10;
+	  
+	  if (a==-10)
+	    {
+	      System.out.println("a is Negative");
+	    }
+	 else
+	    {
+	      System.out.println("a is Positive");
+	    }
+   }
+}
