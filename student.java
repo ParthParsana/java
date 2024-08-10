2a83d766f05184b7b8d9822453dd52ac5036f400
Parth Parsana <145137359+ParthParsana@users.noreply.github.com>
2024-08-10 12:51:07 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,23 @@
+class student
+{
+   public static void main(String ar[])
+   
+   {
+      System.out.println("NAME: Parth Manishbhai Parsana");
+	  System.out.println("EN.No: 206230316040");
+	  System.out.println("MOBILE: 9898147969");
+	  System.out.println("ADDRESS: surat ");
+	  System.out.println();
+	  System.out.println();
+	  System.out.println("NAME: DARSHIL PATEL");
+	  System.out.println("EN.No: 206230316030");
+	  System.out.println("MOBILE: 9898147969");
+	  System.out.println("ADDRESS: surat ");
+	  System.out.println();
+	  System.out.println();
+	  System.out.println("NAME: PANCHAL VARUN");
+	  System.out.println("EN.No: 206230316033");
+	  System.out.println("MOBILE: 9898147969");
+	  System.out.println("ADDRESS: Ahmedabad ");
+   }
+}
