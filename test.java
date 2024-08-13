bc42cf84a67157ec132b9eddf35e866e5d2d47fe
Parth Parsana <145137359+ParthParsana@users.noreply.github.com>
2024-08-13 23:58:43 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,12 @@
+public class test
+{
+	public static void main(String ar[])
+	{
+	   int count =1;
+	   while(count <=15)
+	   {
+	     System.out.println(count % 2==1?"***":"+++++");
+	     ++count;
+	   }
+	}
+}
